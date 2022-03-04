fd4ce752ead9b70d3a08d569d18e877723b3e877
TDP17 <71214045+TDP17@users.noreply.github.com>
2022-03-04 17:51:09 +0530

    fix: Transaction routes and bugfixes (#6)

--- routes/fundsRoutes.js
+++ routes/fundsRoutes.js
@@ -8,6 +8,9 @@ import User_Portfolio from '../models/User_Portfolio.js';
 
 const router = express.Router();
 
+/**
+ * Gets all funds available
+ */
 router.get('/getAll', isAuthorized, async (req, res) => {
     try {
         const funds = await Fund.findAll({});
@@ -26,9 +29,9 @@ router.get('/getAll', isAuthorized, async (req, res) => {
  */
 router.post('/buy', isAuthorized, async (req, res) => {
     const fundId = req.body.id;
-    const fundQty = req.body.qty;
+    const fundQty = +req.body.qty;
     if (fundQty <= 0)
-        res.status(400).json({ error: "Quantity cannot be zero or a negative number" });
+        return res.status(400).json({ error: "Quantity cannot be zero or a negative number" });
     try {
         const fund = await Fund.findByPk(fundId);
 
@@ -40,7 +43,7 @@ router.post('/buy', isAuthorized, async (req, res) => {
                 user.wallet_amount = newAmount;
                 await user.save();
 
-                await Transaction.create({ user_id: user.id, name: fund.name, price: fund.price, qty: fundQty, type: "buy" });
+                await user.createTransaction({ user_id: user.id, name: fund.name, price: fund.price, qty: fundQty, type: "buy" });
 
                 const prevRecord = await User_Portfolio.findOne({ where: { user_id: user.id, fund_id: fund.id } });
                 if (prevRecord) {
@@ -48,8 +51,8 @@ router.post('/buy', isAuthorized, async (req, res) => {
                     prevRecord.qty = newQty;
                     prevRecord.save();
                 }
-                else
-                    await User_Portfolio.create({ user_id: user.id, fund_id: fund.id, qty: fundQty });
+                else await user.addFund({ fund_id: fund.id, qty: fundQty });
+
 
                 res.status(200).json({ message: "Transaction successful" });
             }
@@ -62,25 +65,32 @@ router.post('/buy', isAuthorized, async (req, res) => {
     }
 });
 
+/**
+ * This route receives the id of the fund and the qty to be purchased. 
+ * The username and email are attached to the req object via isAuthorized middleware.
+ * It verifies if the user and fund exist and also if the user has the sufficient qty to sell
+ * SUCCESS @returns 200 and manages the entry on the transaction and portfolio tables
+ * FAILURE @returns 402 - Insufficient Coins, 401 - User/Fund not found, 500
+ */
 router.post('/sell', isAuthorized, async (req, res) => {
     const fundId = req.body.id;
-    const fundQty = req.body.qty;
+    const fundQty = +req.body.qty;
     if (fundQty <= 0)
-        res.status(400).json({ error: "Quantity cannot be zero or a negative number" });
+        return res.status(400).json({ error: "Quantity cannot be zero or a negative number" });
     try {
         const user = await User.findOne({ where: { email: req.email } });
         const fund = await Fund.findOne({ where: { id: fundId } });
         const portfolioEntry = await User_Portfolio.findOne({ where: { user_id: user.id, fund_id: fundId } });
 
-        if (user && fund && portfolioEntry) {
+        if (user && fund) {
             const totalPrice = fund.price * fundQty;
 
-            if (portfolioEntry.qty >= fundQty) {
+            if (portfolioEntry && portfolioEntry.qty >= fundQty) {
                 const newAmount = user.wallet_amount + totalPrice;
                 user.wallet_amount = newAmount;
                 await user.save();
 
-                await Transaction.create({ user_id: user.id, name: fund.name, price: fund.price, qty: fundQty, type: "sell" });
+                await user.createTransaction({ name: fund.name, price: fund.price, qty: fundQty, type: "sell" });
 
                 const newQty = portfolioEntry.qty - fundQty;
                 portfolioEntry.qty = newQty;
